c6c871a8d42006d88c3934112bfdc5ad47567a37
waxx <joaquinsebastianmacol@gmail.com>
2025-09-05 17:02:13 +0800

    Update api.js

--- src/api.js
+++ src/api.js
@@ -8,5 +8,5 @@ export const geoApiOptions = {
 
 export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
 export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
-export const GEOCODING_API_URL = 'http://api.openweathermap.org/geo/1.0';
+export const GEOCODING_API_URL = 'https://api.openweathermap.org/geo/1.0';
 export const WEATHER_API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
